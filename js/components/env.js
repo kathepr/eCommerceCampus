797fdf662facbb57b91f694e4bc250831672f735
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-14 09:01:43 -0500

    feat 🩹 Se corrigen errores

--- js/components/env.js
+++ js/components/env.js
@@ -3,5 +3,5 @@ export const headers = {
 		'x-rapidapi-key': '752d2d2431msh0304c1be5af4024p144590jsnc0c8ebe32de2',
 		'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 	}
-	}
 }
+
--- js/module/app.js
+++ js/module/app.js
@@ -1,34 +1,26 @@
 import { headers } from "../components/env.js";
-export const getAllProductsName = async ({ search: text } = { search: "Phone" }) => {
-    const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${text}&page=1&country=US&sort_by=RELEVANCE&product_condition=ALL `;
+
+export const getAllProductName = async({search:text, id:idCategory})=>{
+    console.log("Esperando .......");
+    console.log(text, idCategory);
+    const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${text}&page=1&country=US&sort_by=RELEVANCE&category_id=${idCategory}&product_condition=ALL`;
     const options = {
         method: 'GET',
         headers
     };
     let res = await fetch(url, options);
-    let data = await res.json();
+    let data = res.json();
     return data;
 }
 
-export const getAllCategory = async () => {
-    const url = 'https://real-time-amazon-data.p.rapidapi.com/product-category-list?country=US';
+export const getAllCategory = async()=>{
+    console.log("Esperando .......");
+    const url = `https://real-time-amazon-data.p.rapidapi.com/product-category-list?country=US`;
     const options = {
         method: 'GET',
         headers
     };
     let res = await fetch(url, options);
-    let data = await res.json();
-    return data;
-}
-
-
-export const getAllProductsByCategory = async ({ search: text, id:idCategory }) => {
-    const url =  `https://real-time-amazon-data.p.rapidapi.com/search?query=${text}&page=1&country=US&sort_by=RELEVANCE&category_id=${idCategory}&product_condition=ALL ` ;
-    const options = {
-        method: 'GET',
-        headers
-    };
-    let res = await fetch(url, options);
-    let data = await res.json();
+    let data = res.json();
     return data;
 }
