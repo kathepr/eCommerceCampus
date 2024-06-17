7eb4c0c8e3d831d757c2f4f0b189dfff3faa15a7
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-17 17:51:30 -0500

    feat ✨ Avances en detail.js

--- js/detail.js
+++ js/detail.js
@@ -1,18 +1,25 @@
-import { galleryCategory } from "./components/gallery.js";
-import { titleProductDetail } from "./components/section.js";
 import { getProductId } from "./module/detail.js";
+import { galleryCategory } from "./components/gallery.js";
+import { footerProductDetail, infomationProductDetail, titleProductDetail } from "./components/section.js";
 
+let mainSectionGallery = document.querySelector('#main__section__gallery');
+let mainSectionTitle = document.querySelector('#main__section__title');
+let mainSectionInformation = document.querySelector('#main__section__information');
+let footerDetail = document.querySelector('#footer__detail');
 
-let main__section_gallery = document.querySelector("#main__section_gallery");
-let main__section_title = document.querySelector("#main__section_title");
-
-addEventListener("DOMContentLoaded", async(e)=>{
+addEventListener("DOMContentLoaded", async e =>{
     let params = new URLSearchParams(location.search);
-    let id = params.get('id');
+    let id = params.get('id')
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductId({id})));
+
+    let title = JSON.parse(localStorage.getItem(id));
     let info = JSON.parse(localStorage.getItem(id));
-    main__section_gallery.innerHTML = await galleryCategory(info)
-    main__section_title.innerHTML = await titleProductDetail(info)
+    let price = JSON.parse(localStorage.getItem(id));
+
+    mainSectionGallery.innerHTML = await galleryCategory(JSON.parse(localStorage.getItem(id)));
+    mainSectionTitle.innerHTML = await titleProductDetail(title);
+    mainSectionInformation.innerHTML = await infomationProductDetail(info);
+    footerDetail.innerHTML = await footerProductDetail(price);
     // let {data} = res;
     // let {
     //     category_path,
